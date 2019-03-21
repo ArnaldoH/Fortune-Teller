b2b32b167a9398880ac3a6c2780f6091a56687df
ArnaldoH <43036590+ArnaldoH@users.noreply.github.com>
2019-03-21 08:47:00 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,38 @@
+var yuh2;
+$(".yuhyuh").click(function(){
+    yuh2 = $(".inp").val();
+    $(".age").html(yuh2);
+    $(".age").html(parseInt(yuh2)*2);
+    $(".married").show();
+    $(".inp").val("");
+});
+
+var yuh1;
+$(".greet").click(function(){
+    yuh1 = $(".inpname").val();
+    $(".nameyuh").show();
+    $(".replace").html(yuh1);
+    $(".inpname").val("");
+});
+
+var yuh3;
+$(".yuhyuhyuh").click(function(){
+    yuh3 = $(".inp2").val();
+    $(".dildo").show();
+    $(".tf").html(yuh3);
+    $(".inp2").val("");
+});
+ 
+var yuh4;
+$(".yuhyuh2").click(function(){
+    yuh4 = $(".inp3").val();
+    $(".L").show();
+    $(".hugeL").html(yuh4);
+    $(".inp3").val("");
+});
+
+
+
+
+
+
